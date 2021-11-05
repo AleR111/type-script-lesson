7ffeb477c2cd2f9c94ae439f78f851279479aa7d
Al <alexseier12345@gmail..com>
2021-11-05 20:16:22 +0300

    render result of search

--- src/search-results.ts
+++ src/search-results.ts
@@ -27,11 +27,36 @@ export function renderEmptyOrErrorSearchBlock(reasonMessage) {
 
 export function renderSearchResultsBlock(data?: Place) {
   console.log(data)
+  let list: string = ''
+
   if (!data) renderBlock('search-results-block',
     `<div class="search-results-header">
             <p>Не удалось найти</p>
           </div>`)
   else {
+    for (const el in data) {
+      list += `<li class="result">
+        <div class="result-container">
+          <div class="result-img-container">
+            <div class="favorites active"></div>
+            <img class="result-img" src="${data[el].image}" alt="">
+          </div>	
+          <div class="result-info">
+            <div class="result-info--header">
+              <p>${data[el].name}</p>
+              <p class="price">${data[el].price}&#8381;</p>
+            </div>
+            <div class="result-info--map"><i class="map-icon"></i> 2.5км от вас</div>
+            <div class="result-info--descr">${data[el].description}</div>
+            <div class="result-info--footer">
+              <div>
+                <button>Забронировать</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </li>`
+    }
     renderBlock(
       'search-results-block',
       `
@@ -47,48 +72,7 @@ export function renderSearchResultsBlock(data?: Place) {
         </div>
     </div>
     <ul class="results-list">
-      <li class="result">
-        <div class="result-container">
-          <div class="result-img-container">
-            <div class="favorites active"></div>
-            <img class="result-img" src="./img/result-1.png" alt="">
-          </div>	
-          <div class="result-info">
-            <div class="result-info--header">
-              <p>YARD Residence Apart-hotel</p>
-              <p class="price">13000&#8381;</p>
-            </div>
-            <div class="result-info--map"><i class="map-icon"></i> 2.5км от вас</div>
-            <div class="result-info--descr">Комфортный апарт-отель в самом сердце Санкт-Петербрга. К услугам гостей номера с видом на город и бесплатный Wi-Fi.</div>
-            <div class="result-info--footer">
-              <div>
-                <button>Забронировать</button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </li>
-      <li class="result">
-        <div class="result-container">
-          <div class="result-img-container">
-            <div class="favorites"></div>
-            <img class="result-img" src="./img/result-2.png" alt="">
-          </div>	
-          <div class="result-info">
-            <div class="result-info--header">
-              <p>Akyan St.Petersburg</p>
-              <p class="price">13000&#8381;</p>
-            </div>
-            <div class="result-info--map"><i class="map-icon"></i> 1.1км от вас</div>
-            <div class="result-info--descr">Отель Akyan St-Petersburg с бесплатным Wi-Fi на всей территории расположен в историческом здании Санкт-Петербурга.</div>
-            <div class="result-info--footer">
-              <div>
-                <button>Забронировать</button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </li>
+        ${list}
     </ul>
     `
     );
